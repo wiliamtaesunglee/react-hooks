9e73377b669a9621219b67c5c355d4bef04f96ba
wil.i.am <wiliam.tae@gmail.com>
2019-07-15 20:52:00 -0300

    tootle img on mouse over

---
+++ pages/imageChangeOnMouseOver.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ImageToggleOnMouseOver from "../src/ImageToggleOnMouseOver";
+
+const ImageChangeOnMouseOver = () => {
+  return (
+    <div>
+      <ImageToggleOnMouseOver primaryImg="" secondaryImg="" alt="" />
+      &nbsp;&nbsp;&nbsp;
+      <ImageToggleOnMouseOver primaryImg="" secondaryImg="" alt="" />
+    </div>
+  );
+};
+
+export default ImageChangeOnMouseOver;
