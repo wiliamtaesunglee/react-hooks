52d8698e6c92a6fdf6de4ec2780efaf576ec5ab3
wil.i.am <wiliam.tae@gmail.com>
2019-07-15 20:25:08 -0300

    added second state

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const InputElement = () => {
+  const [inputText, setInputText] = useState("");
+  const [historyList, setHistoryList] = useState([]);
 
-	const [inputText, setInputText] = useState("");
-
-	return( 
-	<div>
-		<input 
-			onChange={e => {
-				setInputText(e.target.value)
-			}}
-			placeholder="Ender Some Text"/><br/>
-			{inputText}
-	</div>);
+  return (
+    <div>
+      <input
+        onChange={e => {
+          setInputText(e.target.value);
+          setHistoryList([...historyList, e.target.value]);
+        }}
+        placeholder="Ender Some Text"
+      />
+      <br />
+      {inputText}
+      <hr />
+      <br />
+      <ul>
+        {historyList.map(rec => {
+          return <div>{rec}</div>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default InputElement;
-
